9bbdf385870dc7e6fc35db073ec55015235d4419
Chad Smith <Smith.Chad12@gmail.com>
2015-11-11 02:11:26 -0600

    Revert

---
+++ FileDrop.Application/Interfaces/IFileService.cs.BACKUP.22028.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Abp.Application.Services;
+using FileDrop.Domains;
+
+namespace FileDrop.Interfaces
+{
+    public interface IFileService : IApplicationService
+    {
+        /// <summary>
+        /// Gets all files.
+        /// </summary>
+        /// <param name="searchTerm">The search term.</param>
+        /// <returns></returns>
+        ICollection<File> GetAllFiles(string searchTerm);
+
+        /// <summary>
+        /// Gets the file by identifier.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns></returns>
+        File GetFileById(int id);
+
+        /// <summary>
+        /// Saves the file.
+        /// </summary>
+        /// <param name="file">The file.</param>
+        void SaveFile(File file);
+    }
+}
---
+++ FileDrop.Application/Interfaces/IFileService.cs.BASE.21560.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Abp.Application.Services;
+using FileDrop.Domains;
+
+namespace FileDrop.Interfaces
+{
+    public interface IFileService : IApplicationService
+    {
+        /// <summary>
+        /// Gets all files.
+        /// </summary>
+        /// <returns></returns>
+        ICollection<File> GetAllFiles();
+
+        /// <summary>
+        /// Gets the file by identifier.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns></returns>
+        File GetFileById(int id);
+
+        /// <summary>
+        /// Saves the file.
+        /// </summary>
+        /// <param name="file">The file.</param>
+        void SaveFile(File file);
+    }
+}
